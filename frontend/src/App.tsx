04ffef055c9b2cd180aa3ff3fd290aa2e9586a67
MaxGOUET <maxime.gouet1@gmail.com>
2026-02-10 15:17:38 +0100

    suppr tailwind + install sass et route pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,24 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";    
-import logo from "./assets/logo.png";
+import Accueil from "./pages/Accueil.tsx";
+import Login from "./pages/Login.tsx";
+import BackOffice from "./pages/BackOffice.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import {
+  BrowserRouter as Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Accueil />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={}>
-      <Route path="/" element={}>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/connected" element={<BackOffice />} />
+        <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
